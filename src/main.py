45291f89d7b5044b71634e06ed9dc3524b6d96cb
quadseed <contact@quadseed.net>
2023-01-06 01:11:57 +0900

    add python-dotenv

--- src/main.py
+++ src/main.py
@@ -3,19 +3,23 @@ import os
 import pathlib
 from concurrent.futures import ProcessPoolExecutor
 
+import dotenv
 import uvicorn
 
 from server import app
 import utils
 
 def start_api_server():
-    uvicorn.run(app, host='0.0.0.0', port=3880, log_level='info')
+    port = int(os.environ.get("PORT"))
+    uvicorn.run(app, host='0.0.0.0', port=port, log_level='info')
 
 def start_websocket_client():
     pass
 
 def main():
+    dotenv.load_dotenv()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s  [%(levelname)s] (%(name)s) >> %(message)s')
+
     os.chdir(os.path.dirname(pathlib.Path(__file__).parent.resolve()))
     utils.create_output_directory()
     with ProcessPoolExecutor() as executor:
