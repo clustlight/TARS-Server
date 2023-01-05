ed703cd086355f492484d78638e1de5d7e09ba93
quadseed <contact@quadseed.net>
2023-01-05 13:29:25 +0900

    add multiprocessing logic

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from concurrent.futures import ProcessPoolExecutor
+
 import uvicorn
 
 from server import app
+import file
 
+def start_api_server():
+    uvicorn.run(app, host='0.0.0.0', port=3880, log_level='info')
+
+def start_websocket_client():
+    pass
 
 def main():
-    uvicorn.run(app, host='0.0.0.0', port=3880, log_level='info')
+    file.create_output_directory()
+    with ProcessPoolExecutor() as executor:
+        executor.submit(start_api_server)
+        executor.submit(start_websocket_client)
 
 
 if __name__ == "__main__":
