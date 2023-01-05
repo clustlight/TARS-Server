f7763ab3759f0b6c84d362360be33cc27f87aed4
quadseed <contact@quadseed.net>
2023-01-05 13:38:06 +0900

    update /records

--- server.py
+++ server.py
@@ -10,17 +10,27 @@ app = FastAPI()
 async def root():
     return {"message": "OK"}
 
+@app.get("/records")
+async def get_records():
+    record_flags = stream_manager.events
+    keys = [key for key, value in record_flags.items() if not value.is_set()]
+    response = []
+    for index, key in enumerate(keys):
+        record = {"user_name": key}
+        response.append(record)
 
-@app.post("/record/{user_name}")
-async def start_record(user_name):
+    return {"records": response}
+
+@app.post("/records/{user_name}")
+async def start_record(user_name: str):
     if stream_manager.start(user_name):
         return {"user": user_name}
     else:
         return {"error": "recording is on going..."}
 
 
-@app.delete("/record/{user_name}")
-async def stop_record(user_name):
+@app.delete("/records/{user_name}")
+async def stop_record(user_name: str):
     if stream_manager.stop(user_name):
         return {"user": user_name}
     else:
