a9418186d342ba9cfa2d6a673fb39a7aa8fb25d2
quadseed <contact@quadseed.net>
2023-01-07 15:36:56 +0900

    change placeholder

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ def create_user_directory(user_name):
         os.mkdir(f"./outputs/{user_name}")
 
 def replace_colon(user_name):
-    return user_name.replace(':', '_')
+    return user_name.replace(':', '__')
 
 def get_archive_file_name(movie_id, user_name, live_title, live_subtitle):
     now = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
