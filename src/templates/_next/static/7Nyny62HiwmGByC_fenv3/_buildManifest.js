1306f0ac06b3f30cb6163bbbfb89cbe0652119cc
quadseed <contact@quadseed.net>
2023-04-26 07:54:46 +0900

    fix endpoint

--- src/templates/_next/static/7Nyny62HiwmGByC_fenv3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/459-3bbc2c5576827e3f.js","static/chunks/pages/index-2ee5371b24d5f91c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
