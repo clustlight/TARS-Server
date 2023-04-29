01329e1a1eb131e9cd4727ca6b4bbbbc7aa781a3
quadseed <contact@quadseed.net>
2023-04-29 23:00:25 +0900

    add debug logger for fetch_scheduler

--- src/parallel.py
+++ src/parallel.py
@@ -131,20 +131,25 @@ async def stream_notification(url):
 
 
 def fetch_scheduler():
+    logger = logging.getLogger("Subscriptions")
     sleep(3)
     twitcasting = Twitcasting()
 
     while True:
         fetch_count = 0
+        logger.debug("Retrieving subscriptions...")
         response = requests.get(f"http://localhost:{port}/subscriptions")
         users = response.json()["users"]
+        logger.debug(f"{len(users)} user(s) found")
         for user in users:
             database.set_subscription_user(user)
 
         for user in users:
             fetch_count += 1
+            logger.debug(f"Retrieving user_id: [{user}]'s metadata...")
             user_data_response = twitcasting.get_user_info(user)
             if user_data_response[0]:
+                logger.debug(f"{user} is {user_data_response[1]['user']['screen_id']}")
                 database.update_user(user_data_response[1])
 
             if fetch_count == 5:
